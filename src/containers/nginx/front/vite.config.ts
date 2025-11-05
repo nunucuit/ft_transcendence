e8339c6b2e6dac4c23a753c28d671aa02b24dea2
Gabriel Sanchez <gabrsanc@c1r4p6.42nice.fr>
2025-11-05 13:14:36 +0100

    browser compatibility almost done

--- src/containers/nginx/front/vite.config.ts
+++ src/containers/nginx/front/vite.config.ts
@@ -1,15 +1,23 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vite.dev/config/
 export default defineConfig({
-	appType: 'spa',
-	plugins: [react(), tailwindcss()],
-	resolve: {
+  appType: "spa",
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
