692425f5f05888182c482878b877cd22faf7347d
Gabriel Sanchez <gabrsanc@c3r1p1.42nice.fr>
2025-11-07 17:28:08 +0100

    removed useless files

--- src/containers/nginx/front/vite.config.ts
+++ src/containers/nginx/front/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
 
-// https://vite.dev/config/
 export default defineConfig({
   appType: "spa",
   plugins: [
